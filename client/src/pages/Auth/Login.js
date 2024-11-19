3d5608e210b0f8b79e98107e64bd117e344365ac
Hetvi-j <167184830+Hetvi-j@users.noreply.github.com>
2024-11-19 15:19:35 +0530

    Update Login.js

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://fashionflux.onrender.com/api/v1/auth/login", {
+      const res = await axios.post("http://fashionflux.onrender.com/api/v1/auth/login", {
         email,
         password,
       });
